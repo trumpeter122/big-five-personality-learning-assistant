928cfe82eee2ed01620f35010a71bd4cffc11787
trumpeter122 <186272455+trumpeter122@users.noreply.github.com>
2025-12-14 16:32:30 +0800

    fix: fix results page url

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/big-five-personality-learning-assistant/',
+  base: process.env.NODE_ENV === 'production' ? '/big-five-personality-learning-assistant/' : '/',
 })
